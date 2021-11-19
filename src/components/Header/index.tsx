d5efc9022a738d29ba140698490317437105a66f
Carlos Henrique <54608090+carlos0406@users.noreply.github.com>
2021-11-19 14:01:41 -0300

    adicionado atualização de tabela

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
 import { Header } from "./styles";
 import Link from 'next/link'
+import { api } from "../../services/api";
+import { toast } from "react-toastify";
 export function HeaderComponent() {
+  async function atualizarPlanilha(){
+   const {status}= await api.post('/registros',[]);
+   if(status===201){
+    toast.success(`Planilha atualizada com sucesso!`);
+   }
+  }
   return (
     <Header>
+      <button onClick={atualizarPlanilha}>
+        Atualizar planilha
+      </button>
       <div>
       <img src="/logo.svg" alt="" />
       </div>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,22 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
+button{
+            background-color:#33CC95;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            font-weight: bold;
+            font-size: 1.25rem;
+            padding: 1rem;
+            position: relative;
+            top: 3rem;
+            left: 1rem;
+            transition: filter 0.2s ;
+            &:hover{
+              filter: brightness(90%);
+            }
+        }
   div {
     width: 1180px;
     margin:0 auto;
